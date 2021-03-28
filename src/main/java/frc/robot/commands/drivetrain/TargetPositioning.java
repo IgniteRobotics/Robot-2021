aaff5408ce5dc2ab60edf1e97933a74ff89b70c1
Josh Reddick <josh.reddick@coxautoinc.com>
2021-03-27 20:10:55 -0400

    wip on targeting

--- src/main/java/frc/robot/commands/drivetrain/TargetPositioning.java
+++ src/main/java/frc/robot/commands/drivetrain/TargetPositioning.java
@@ -11,8 +11,14 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.RamseteDriveSubsystem;
+
+import java.util.Map;
+
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.util.VisionUtils;
 
@@ -20,12 +26,18 @@ import frc.robot.util.VisionUtils;
 public class TargetPositioning extends CommandBase {
   private static NetworkTableInstance inst = NetworkTableInstance.getDefault();
   private static NetworkTable table = inst.getTable("limelight");
-  private static double KpTurn = 0.05;
+  private static double KpTurn = 0.02;
   private static double min_command = 0.05;
   // the range you want.
   //allowed margin of errorit 
   private double marginOfErrorTurn = 2.0;
+
+  private ShuffleboardTab tab;
+  private NetworkTableEntry kpTurnEntry;
+
+  boolean targetFound = false;
   
+
   
   private final RamseteDriveSubsystem m_driveTrain;
   /**
@@ -35,11 +47,17 @@ public class TargetPositioning extends CommandBase {
     addRequirements(driveTrain);
     this.m_driveTrain = driveTrain;
     // Use addRequirements() here to declare subsystem dependencies.
+
+    tab = Shuffleboard.getTab("Limelight");
+    kpTurnEntry = tab.add("kPTurn Limelight", KpTurn).withProperties(Map.of("min", 0)).getEntry();
+
   }
 
   // Called wen the command is initially scheduled.
   @Override
   public void initialize() {
+    targetFound = false;
+    KpTurn = kpTurnEntry.getDouble(KpTurn);
     table.getEntry("camMode").setNumber(0);
     table.getEntry("ledMode").setNumber(3);
   }
@@ -48,33 +66,38 @@ public class TargetPositioning extends CommandBase {
   @Override
   public void execute() {
     
-    double ty = (double) table.getEntry("ty").getNumber(0);
+    
     double tv = (double) table.getEntry("tv").getNumber(0);
+    SmartDashboard.putNumber("Limelight tv", tv);
 
-    SmartDashboard.putNumber("ty", ty);
-    SmartDashboard.putNumber("tv", tv);
-    
-    
+    if (tv > 0) {
+      
+      targetFound = true;
+
+      double tx = (double) table.getEntry("tx").getNumber(0);
+      double ty = (double) table.getEntry("ty").getNumber(0);
+
+      SmartDashboard.putNumber("Limelight tx", tx);
+      SmartDashboard.putNumber("Limelight ty", ty);
+      
+      double headingError = -tx;
+      double steeringAdjust = 0.0;
+      if (tx > 1.0)
+      {
+              steeringAdjust =  KpTurn*headingError - min_command;
+      }
+      else if (tx < 1.0)
+      {
+              steeringAdjust = KpTurn*headingError + min_command;
+      }
 
 
-
-
-    double tx = (double) table.getEntry("tx").getNumber(0);
-    double headingError = -tx;
-    double steeringAdjust = 0.0;
-    if (tx > 1.0)
-    {
-             steeringAdjust =  KpTurn*headingError - min_command;
+      //m_driveTrain.arcadeDrive(-drivingAdjust,steeringAdjust,Constants.kDriveDeadband);
+      //flip it since the shooter is on the back.
+      m_driveTrain.arcadeDrive(0,-steeringAdjust,true);
+    } else {
+      targetFound = false;
     }
-    else if (tx < 1.0)
-    {
-            steeringAdjust = KpTurn*headingError + min_command;
-    }
-
-
-    //m_driveTrain.arcadeDrive(-drivingAdjust,steeringAdjust,Constants.kDriveDeadband);
-    //flip it since the shooter is on the back.
-    m_driveTrain.arcadeDrive(0,-steeringAdjust,false);
   }
 
   // Called once the command ends or is interrupted.
@@ -89,6 +112,6 @@ public class TargetPositioning extends CommandBase {
   public boolean isFinished() {
     double tx = (double) table.getEntry("tx").getNumber(0);
     boolean yawOK = Math.abs(tx) <= marginOfErrorTurn;
-    return yawOK;
+    return yawOK && targetFound;
   }
 }
