4a00c1a55c03257b47d1e0743e564b39e33cf5ab
Jaeheon Shim <git@jaeheonshim.com>
2021-06-26 14:57:58 -0400

    Add rotation command with wheel encoders

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -9,6 +9,7 @@ import java.time.Duration;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 
