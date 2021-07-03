02516d8f194aeabfa0521d05d31e726b9d6aa171
rossydang <rossy.d@igniterobotics.com>
2021-07-03 13:39:12 -0400

    Climb Motion Magic

--- src/main/java/frc/robot/commands/ClimbMotionMagicDown.java
+++ src/main/java/frc/robot/commands/ClimbMotionMagicDown.java
@@ -7,12 +7,15 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Climber;
 
-public class ClimbDown extends CommandBase {
+
+public class ClimbMotionMagicDown extends CommandBase {
   private Climber climb;
-  /** Creates a new ClimbUp. */
-  public ClimbDown(Climber climb) {
+  private int targetPositionTicks = 10000;
+  /** Creates a new ClimbMotionMagicUp. */
+  public ClimbMotionMagicDown(Climber climb) {
     // Use addRequirements() here to declare subsystem dependencies.
-     this.climb = climb;
+    this.climb = climb;
+    addRequirements(climb);
   }
 
   // Called when the command is initially scheduled.
@@ -22,14 +25,12 @@ public class ClimbDown extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    climb.goDown();
+    climb.setMotionMagicPosition(targetPositionTicks);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-   // climberLeader.setSelectedSensorPosition(0, kPIDLoopIdx, kTimeoutMs);
-    climb.stop();
     climb.zeroEncoders();
   }
 
--- src/main/java/frc/robot/commands/ClimbMotionMagicUp.java
+++ src/main/java/frc/robot/commands/ClimbMotionMagicUp.java
@@ -7,12 +7,15 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Climber;
 
-public class ClimbDown extends CommandBase {
+
+public class ClimbMotionMagicUp extends CommandBase {
   private Climber climb;
-  /** Creates a new ClimbUp. */
-  public ClimbDown(Climber climb) {
+  private int targetPositionTicks = 210000;
+  /** Creates a new ClimbMotionMagicUp. */
+  public ClimbMotionMagicUp(Climber climb) {
     // Use addRequirements() here to declare subsystem dependencies.
-     this.climb = climb;
+    this.climb = climb;
+    addRequirements(climb);
   }
 
   // Called when the command is initially scheduled.
@@ -22,16 +25,12 @@ public class ClimbDown extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    climb.goDown();
+    climb.setMotionMagicPosition(targetPositionTicks);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-   // climberLeader.setSelectedSensorPosition(0, kPIDLoopIdx, kTimeoutMs);
-    climb.stop();
-    climb.zeroEncoders();
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
