809173bba0e1e383857549bc4b0d5be19520d5e7
Jaeheon Shim <git@jaeheonshim.com>
2021-07-04 22:53:08 -0400

    Only initialize BadLog when robot isReal

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,7 +36,10 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    initLogging();
+    if(Robot.isReal()) {
+      initLogging();
+    }
+
     m_robotContainer = new RobotContainer();
     LiveWindow.disableAllTelemetry();
   }
