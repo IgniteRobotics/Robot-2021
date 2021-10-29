55e027025b700ad7d6c1352d190b644935d7370f
IgniteRobotics <ignite@igniterobtics.com>
2021-10-29 08:46:57 -0400

    Fix sixball auton to use centered

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -134,8 +134,8 @@ public class RobotContainer {
   SequentialCommandGroup(
     new ResetHood(m_shooter),
     new SetIntake(m_intake, false),
-    new ShootInterpolatedBall(m_shooter, m_indexer, m_limelight).withTimeout(2.5),
-    new TurnAngle(m_driveTrain, 0).withTimeout(1)
+    new ShootInterpolatedBall(m_shooter, m_indexer, m_limelight).withTimeout(2.5)
+    // new TurnAngle(m_driveTrain, 0).withTimeout(1)
   );
 
   private SequentialCommandGroup sixBallAuton;
@@ -204,7 +204,7 @@ public class RobotContainer {
   }
 
   public void configureAutonCommand() {
-    Trajectory forwards = loadTrajectory("TrenchForwards");
+    Trajectory forwards = loadTrajectory("Centered");
     Trajectory backwards = loadTrajectory("TrenchBackwards");
 
     DriveTrajectory forwardsDrive = new DriveTrajectory(m_driveTrain, forwards);
@@ -220,7 +220,7 @@ public class RobotContainer {
       new ParallelCommandGroup(group, new ResetHood(m_shooter)),
       backwardsDrive,
       new TargetPositioning(m_driveTrain, m_driveController, m_limelight).withTimeout(1.5),
-      new ShootBallSpecific(m_shooter, m_indexer, 5800, 1600)
+      new ShootInterpolatedBall(m_shooter, m_indexer, m_limelight)
     );
 
     this.chooseAuton.addOption("SixBall Auton", sixBallAuton);
